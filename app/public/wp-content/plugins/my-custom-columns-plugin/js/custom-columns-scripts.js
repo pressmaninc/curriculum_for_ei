b7fb18d3c37270f68f18d1cbac824d33e759cbac
EiThandarAung-pressman <144673855+EiThandarAung-pressman@users.noreply.github.com>
2023-09-22 14:06:21 +0900

    Your commit message

--- app/public/wp-content/plugins/my-custom-columns-plugin/js/custom-columns-scripts.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-    // Example: Highlight rows with a word count over 10
-    $('.column-word_count').each(function() {
-        var wordCount = parseInt($(this).text());
-        if (wordCount > 10) {
-            $(this).closest('tr').css('background-color', '#e6f7ff');
-        }
-    });
-});
